87075af4ed73c8d4eac9313278ad56c831e034c8
HARUKI1127 <26urakami@nao.net.it-chiba.ac.jp>
2025-11-25 22:05:18 +0900

    MySQLの追加

--- src/main/java/controller/MainServlet.java
+++ src/main/java/controller/MainServlet.java
@@ -13,6 +13,7 @@ import javax.servlet.http.HttpServletResponse;
 import model.Mutter;
 import model.MutterDAO;
 
+
 @WebServlet("/Main")
 public class MainServlet extends HttpServlet {
     private static final long serialVersionUID = 1L;
